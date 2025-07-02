05781e84e5f5d13b69a5a13a641f80e3c89b5df2
Masayoshi Nakazeko <nakaseko_m@abeam.ocn.ne.jp>
2025-07-02 18:12:31 +0900

    Update Test_mysql_01.py

--- Test_mysql_01.py
+++ Test_mysql_01.py
@@ -3,7 +3,7 @@ import mysql.connector
 # 接続定義
 sql_host = 'localhost'
 sql_user = 'root'
-sql_password = 'Seko19670614'
+sql_password = '************'
 sql_database = 'mysql'
 
 # テーブル名とカラム名（例: Name, Time_zone_id）
